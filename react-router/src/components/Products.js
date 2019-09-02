c5d3af1361e70a8f1c35275045e46498c8f34222
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-02 11:53:25 +0700

    match p2

--- react-router/src/components/Products.js
+++ react-router/src/components/Products.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
+import Product from './Product';
 
 class Products extends Component {
     render() {
         var products = [
             {
                 id: 1,
+                slug: 'apple',
                 name: 'iPhone X',
                 price: 35000000
             },
             {
                 id: 2,
+                slug: 'apple2',
                 name: 'iPhone XS Max',
                 price: 40000000
             },
             {
                 id: 3,
+                slug: 'apple3',
                 name: 'iPhone 11',
                 price: 45000000
             }
         ];
 
-        var { match } = this.props;
-        console.log(match);
+        var { match } = this.props; //this.props.match
+        var url = match.url;
 
         var result = products.map((product, index) => {
             return (
-                <NavLink to="" key={index}>
+                <NavLink to={`${url}/${product.slug}`} key={index}>
                     <li className="list-group-item" >
                         { product.id } - { product.name } - { product.price }
                     </li>
@@ -37,11 +41,13 @@ class Products extends Component {
         return(
             <div className="container">
                 <h1>Danh sách sản phẩm</h1>
+                <div className="row">
                     <ul className="list-group">
                         { result }
                     </ul>
+                </div>
                 <div className="row">
-                    
+                    <Route path="/products/:name" component={Product} />
                 </div>
             </div>
         )
