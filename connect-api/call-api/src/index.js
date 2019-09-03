10ae5cf1f7e336a11997c0a63aff5fb19d1a15a9
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-03 21:17:56 +0700

    connect redux

--- connect-api/call-api/src/index.js
+++ connect-api/call-api/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import appReducers from './reducers/index';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    appReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+    
 serviceWorker.unregister();
---
+++ connect-api/call-api/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import products from './products.js';
+
+const appReducers = combineReducers({
+    products
+});
+
+export default appReducers;
