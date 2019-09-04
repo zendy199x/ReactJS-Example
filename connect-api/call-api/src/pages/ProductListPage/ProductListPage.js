9fb052faee8c02ee60a8a5efdc0fbc6bf1d7eb8f
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-04 19:46:00 +0700

    using axios call api

--- connect-api/call-api/src/pages/ProductListPage/ProductListPage.js
+++ connect-api/call-api/src/pages/ProductListPage/ProductListPage.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 import ProductList from './../../components/ProductList/ProductList';
 import ProductItem from './../../components/ProductItem/ProductItem';
 import { connect } from 'react-redux';
-import axios from 'axios';
+import callApi from './../../utils/apiCaller';
 
 class ProductListPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products : []
+        };
+    }
+
+    componentDidMount() { //chạy sau khi hàm render được chạy lần đầu tiên
+        callApi('products', 'GET', null).then(res => {
+            this.setState({
+                products: res.data
+            })
+        })
+    }
+
     render() {
         // var { products } = this.props;
-        var products = [];
-        axios({
-            method: 'GET',
-            url: 'http://localhost:3000/products',
-            data: null
-        }).then(res => {
-            console.log(res);
-            product = res.data;
-        }).catch(err => {
-            console.log(err);
-        })
+        var { products } = this.state;
 
         return(
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
