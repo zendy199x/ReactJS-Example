940733c37efb397033bcc24b702e2a1adff6accb
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-04 17:29:01 +0700

    axios

--- connect-api/call-api/src/pages/ProductListPage/ProductListPage.js
+++ connect-api/call-api/src/pages/ProductListPage/ProductListPage.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import ProductList from './../../components/ProductList/ProductList';
 import ProductItem from './../../components/ProductItem/ProductItem';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 class ProductListPage extends Component {
     render() {
-        var { products } = this.props;
+        // var { products } = this.props;
+        var products = [];
+        axios({
+            method: 'GET',
+            url: 'http://localhost:3000/products',
+            data: null
+        }).then(res => {
+            console.log(res);
+            product = res.data;
+        }).catch(err => {
+            console.log(err);
+        })
+
         return(
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <button type="button" className="btn btn-info mb-10">
