e9377fa05a78e1e901eb3d3ff92739da3e95fc2f
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-05 10:30:15 +0700

    add product p1

--- connect-api/call-api/src/pages/ProductActionPage/ProductActionPage.js
+++ connect-api/call-api/src/pages/ProductActionPage/ProductActionPage.js
@@ -1,27 +1,75 @@
 import React, { Component } from 'react';
 import ProductList from './../../components/ProductList/ProductList';
 import ProductItem from './../../components/ProductItem/ProductItem';
+import callApi from './../../utils/apiCaller';
 
 class ProductActionPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            txtName: '',
+            txtPrice: '',
+            chkbStatus: ''
+        };
+    }
+
+    onChange = (e) => {
+        var target = e.target;
+        var name = target.name;
+        var value = target.type === 'checkbox' ? target.checked : target.value;
+        this.setState({
+            [name] : value
+        })
+    }
+    onSave = (e) => {
+        e.preventDefault();
+        var {txtName, txtPrice, chkbStatus} = this.state;
+        callApi('products', 'POST', {
+            name: txtName,
+            price: txtPrice,
+            status: chkbStatus
+        }).then(res => {
+            console.log(res);
+        })
+    }
+    
     render() {
-        var products = [];
+        var {txtName, txtPrice, chkbStatus} = this.state;
         return(
             <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <form>
+                <form onSubmit={this.onSave}>
                     <div className="form-group">
                         <label for="">Tên sản phẩm</label>
-                        <input type="text" className="form-control"/>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="txtName"
+                            value={txtName}
+                            onChange={this.onChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label for="">Giá sản phẩm</label>
-                        <input type="number" className="form-control"/>
+                        <input
+                            type="number"
+                            className="form-control"
+                            name="txtPrice"
+                            value={txtPrice}
+                            onChange={this.onChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label for="">Trạng thái</label>
                     </div>
                     <div className="checkbox">
                         <label>
-                            <input type="checkbox"/>
+                            <input
+                                type="checkbox"
+                                name="chkbStatus"
+                                value={chkbStatus}
+                                onChange={this.onChange}
+                            />
                             Còn hàng
                         </label>
                     </div>
--- connect-api/call-api/src/pages/ProductListPage/ProductListPage.js
+++ connect-api/call-api/src/pages/ProductListPage/ProductListPage.js
@@ -3,6 +3,7 @@ import ProductList from './../../components/ProductList/ProductList';
 import ProductItem from './../../components/ProductItem/ProductItem';
 import { connect } from 'react-redux';
 import callApi from './../../utils/apiCaller';
+import { Link } from 'react-router-dom'
 
 class ProductListPage extends Component {
     constructor(props) {
@@ -26,9 +27,9 @@ class ProductListPage extends Component {
 
         return(
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <button type="button" className="btn btn-info mb-10">
+                <Link to='/product/add' className="btn btn-info mb-10">
                     Thêm sản phẩm
-                </button>
+                </Link>
                 <ProductList>
                     { this.showProducts(products) }
                 </ProductList>
