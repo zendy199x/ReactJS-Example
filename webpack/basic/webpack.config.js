35f96332ec992ab8007f00cf650321fc51d699e1
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-07 13:20:43 +0700

    start webpack

---
+++ webpack/basic/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    }
+}
+
+module.exports = config;
