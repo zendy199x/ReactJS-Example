4239d34a83706ba80000e5d269f7c1ae4e9b6db6
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-07 16:44:55 +0700

    add material ui

---
+++ redux-saga/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Redux Saga</h1>
+      <Button variant="contained" color="primary">Material UI Button</Button>
+    </div>
+  );
+}
+
+export default App;
