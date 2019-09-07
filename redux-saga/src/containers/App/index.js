e315b0f4c7b37733c9415e68430c8aef5618ac03
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-07 17:39:14 +0700

    material ui theme

--- redux-saga/src/containers/App/index.js
+++ redux-saga/src/containers/App/index.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import styles from './styles'
 import { withStyles } from '@material-ui/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import Taskboard from './../Taskboard/index';
+import theme from './../../commons/Theme';
 
 class App extends Component {
     render() {
-        const { classes } = this.props;
         return (
-            <div className="App">
-                <h1>Hello Redux Saga</h1>
-                <Button variant="contained" color="primary">Material UI Button</Button>
-                <div className={classes.box}>
-                    <div className={classes.shape}>ReactJS</div>
-                    <div className={classes.shape}>VueJS</div>
-                    <div className={classes.shape}>AngularJS</div>
-                </div>
-            </div>
+            <ThemeProvider theme={theme}>
+                <Taskboard />
+            </ThemeProvider>
         );
     }
 }
