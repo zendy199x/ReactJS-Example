144a89e716b6a587f035a483063150e0a71703ea
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-09 18:04:06 +0700

    build interface p2

--- redux-saga/redux-saga-material/src/containers/Taskboard/index.js
+++ redux-saga/redux-saga-material/src/containers/Taskboard/index.js
@@ -5,18 +5,72 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import Grid from '@material-ui/core/Grid';
 import { STATUSES } from './../../constains/';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+const listTask = [
+    {
+        id: 1,
+        title: "Read book",
+        description: "React Material UI book",
+        status: 0
+    },
+    {
+        id: 2,
+        title: "Play soccer",
+        description: "Play",
+        status: 2
+    },
+    {
+        id: 3,
+        title: "Online Facebook",
+        description: "React Material UI book",
+        status: 1
+    }
+]
 
 class Taskboard extends Component {
 
     renderBoard() {
+        const { classes } = this.props;
         let xhtml = null;
         xhtml = (
             <Grid container spacing={2}>
                 {
                     STATUSES.map((status, index) => {
+                        const taskFiltered = listTask.filter(task => task.status === status.value);
                         return (
                             <Grid item md={4} xs={12} key={index}>
-                                {status.label}
+                                <Box mt={2} mb={2}>
+                                    <div className={classes.status}>{status.label}</div>
+                                </Box>
+                                <div className={classes.wrapperListTask}>
+                                    {taskFiltered.map(task => {
+                                        const { title } = task;
+                                            return (
+                                                <Card key={task.id} className={classes.card}>
+                                                    <CardContent>
+                                                        <Grid container justify="space-between">
+                                                            <Grid item md={8}>
+                                                                <Typography component="h2">
+                                                                    {title}
+                                                                </Typography>
+                                                            </Grid>
+                                                            <Grid item md={4}>
+                                                                {status.label}
+                                                            </Grid>
+                                                        </Grid>
+                                                    </CardContent>
+                                                    <CardActions>
+                                                        <Button size="small" />
+                                                    </CardActions>
+                                                </Card>
+                                            )
+                                        })}
+                                </div>
                             </Grid>
                         )
                     })
