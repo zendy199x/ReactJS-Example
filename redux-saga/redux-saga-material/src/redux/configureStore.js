43062e437f5c42df27003c9700eec4e6e3c77422
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-09-17 23:56:56 +0700

    add redux-saga

--- redux-saga/redux-saga-material/src/redux/configureStore.js
+++ redux-saga/redux-saga-material/src/redux/configureStore.js
@@ -1,6 +1,8 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './../reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './../sagas';
 
 const composeEnhancers = 'process.env.NODE_ENV' !== 'production' &&
     typeof window === 'object' &&
@@ -8,11 +10,16 @@ const composeEnhancers = 'process.env.NODE_ENV' !== 'production' &&
             shouldHotReload: false
         }) : compose;
 
+const sagaMiddleware = createSagaMiddleware()
+
 const configureStore = () => {
-    const middlewares = [thunk];
+    const middlewares = [thunk, sagaMiddleware];
     const enhancers = [applyMiddleware(...middlewares)];
     const store = createStore(rootReducer, composeEnhancers(...enhancers));
+    sagaMiddleware.run(rootSaga);
     return store;
 };
+
+
 
 export default configureStore;
