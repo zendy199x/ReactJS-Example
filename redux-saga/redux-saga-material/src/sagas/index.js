f4967e294d4a7395dca6a695ac033d2c2f9162f1
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-10-02 10:15:24 +0700

    redux saga put

--- redux-saga/redux-saga-material/src/sagas/index.js
+++ redux-saga/redux-saga-material/src/sagas/index.js
@@ -1,21 +1,26 @@
-import { fork, take, call } from 'redux-saga/effects';
+import { fork, take, call, put } from 'redux-saga/effects';
+import { fetchListTaskSuccess, fetchListTaskFailed } from '../actions/task';
 import * as taskTypes from './../constants/task';
 import { getList } from './../apis/task';
 import { STATUS_CODE } from './../constants';
 
 function* watchFetchListTaskAction() {
-    yield take(taskTypes.FETCH_TASK);
-    const resp  = yield call(getList);
-    const { status, data} = resp;
-    if(status === STATUS_CODE.SUCCESS) {
-    //dispatch action fetchListTaskSuccess
-    } else {
-    //dispatch action fetchListTaskSuccess fail
+    while (true) {
+        yield take(taskTypes.FETCH_TASK);
+        const resp = yield call(getList);
+        const { status, data } = resp;
+        if (status === STATUS_CODE.SUCCESS) {
+            //dispatch action fetchListTaskSuccess
+            yield put(fetchListTaskSuccess(data));
+        } else {
+            //dispatch action fetchListTaskSuccess fail
+            yield put(fetchListTaskFailed(data));
+        }
     }
 }
 
 function* watchCreateTaskAction() {
-    console.log('watch create task action')
+    console.log('watch create task action');
 }
 
 function* rootSaga() {
